5c5bd1a684269911362277f0dc5df5cd019d6f38
markelom <33352064+markelom@users.noreply.github.com>
2017-11-08 21:26:20 +0700

    From 8.11

--- Assignment01.js
+++ Assignment01.js
@@ -1,58 +1,52 @@
-var http = require("http").createServer(handler);
-var io = require("socket.io").listen(http);
-var fs = require("fs");//variable for file system
+var http = require("http").createServer(handler); // on req - handler - function to handle events
+var io = require("socket.io").listen(http); // socket library
+var fs = require("fs"); // variable for file system for providing html
 var firmata = require("firmata");
 
-var board = new firmata.Board("/dev/ttyACM0", function(){ // ACM Abstract Control Model for serial communication with Arduino (could be USB)
+console.log("Starting the code");
+
+var board = new firmata.Board("/dev/ttyACM0", function(){
     console.log("Connecting to Arduino");
-    console.log("Activation of Pin 8");
-    board.pinMode(8, board.MODES.OUTPUT); // Configures the specified pin to behave either as an input or an output.
     console.log("Activation of Pin 13");
-    board.pinMode(13, board.MODES.OUTPUT); // Configures the specified pin to behave either as an input or an output.
+    board.pinMode(13, board.MODES.OUTPUT); // pin13 as out
 });
 
-
-function handler(req, res){
+// we should define handler function which is an argument in the first line
+// to specify how events are handled
+// handler is similar function as in example01.js -> http.createServer(function(req, res){...
+function handler(req, res) {
     fs.readFile(__dirname + "/Assignment01.html",
     function (err, data) {
         if (err) {
-            res.writeHead(500,{"Content-Type": "text/plain"});
-            return res.end("Error loading HTML page.");
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            return res.end("Error loading html page.");
         }
-        res.writeHead(200);
-        res.end(data);
-    });
-    
+    res.writeHead(200);
+    res.end(data);
+    })
 }
 
 var timer1;
 
+http.listen(8080); // server will listen on port 8080
 
-
-http.listen(8080);// server will listen on port 8080
-
-io.sockets.on("connection", function(socket){
-  socket.on("commandToArduino", function(commandNo){
-     if (commandNo == "0") {
-            board.digitalWrite(13, board.LOW); // write LOW on pin 13
-        }
+io.sockets.on("connection", function(socket) {
+    socket.on("commandToArduino", function(commandNo){
         if (commandNo == "1") {
-            board.digitalWrite(13, board.HIGH); // write HIGH on pin 13
+            board.digitalWrite(13, board.HIGH); // write high on pin 13
         }
-        if (commandNo == "2") {
-            board.digitalWrite(8, board.LOW); // write LOW on pin 8
+        if (commandNo == "0") {
+            board.digitalWrite(13, board.LOW); // write low on pin 13
         }
-        if (commandNo == "3") {
-            board.digitalWrite(8, board.HIGH); // write HIGH on pin 8
-        }        
-  });
+    });
     socket.on("blinkOn", function(){
     board.digitalWrite(13, board.HIGH); // write high on pin 13
     timer1 = setTimeout(function () {board.digitalWrite(13, board.LOW);}, 1000);
     });
-    
     socket.on("blinkOff", function(){
     board.digitalWrite(13, board.LOW);
         clearTimeout(timer1);
     });
+
+
 });
